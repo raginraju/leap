ee28326f665494593e854e27cc4a5b04feb5527d
raginraju <85422179+raginraju@users.noreply.github.com>
2024-05-25 16:51:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from predict_page import show_predict_page
+from pages.predict_page import show_predict_page
 #from explore_page import show_explore_page
 show_predict_page()
 
